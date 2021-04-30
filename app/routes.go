790bfefdbdc2ee881988bf1b867e983597554a4b
Rasmus Nilsson <info@rasmusnilsson.se>
2021-04-30 14:01:13 +0200

    added cors

--- app/routes.go
+++ app/routes.go
@@ -2,10 +2,13 @@ package app
 
 import (
 	"github.com/bee-well/auth/controllers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func mapRoutes(e *gin.Engine) {
+	e.Use(cors.Default())
+
 	e.GET("/marco", controllers.Marco)
 	e.POST("/sign-in", controllers.SignIn)
 	e.POST("/sign-up", controllers.SignUp)
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.7.1
 	github.com/lib/pq v1.10.0
 	github.com/spf13/viper v1.7.1
